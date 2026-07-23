2039ad866aa74bfd8988c006adeba908eb69c7cf
Aarav goyal <aravgoyal791@gmail.com>
2026-07-23 22:06:12 +0530

    meal.py

--- week 1/meal.py
+++ week 1/meal.py
@@ -1,19 +1,19 @@
 def main():
-    time= input("what time is it?")
-    time= convert(time)
-    
-    if 7<= time <= 8:
-            print("breakfast time")
+    time = convert(input("What time is it? "))
+
+    if 7 <= time <= 8:
+        print("breakfast time")
     elif 12 <= time <= 13:
-            print("lunch time")
+        print("lunch time")
     elif 18 <= time <= 19:
-             print("dinner time")
+        print("dinner time")
 
-    def convert(time):
-           hours,minutes=time.split(":")
-           hours= int(hours)
-           minutes=int(minutes)
 
-           return hours+minutes/60
+def convert(time):
+    hours, minutes = time.split(":")
+    return int(hours) + int(minutes) / 60
 
+
+if __name__ == "__main__":
     main()
+   
