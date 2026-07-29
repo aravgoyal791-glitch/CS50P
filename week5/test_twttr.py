1b45c3c1eb6312cfb6e72e61afb4f2bb9928d6de
Aarav goyal <aravgoyal791@gmail.com>
2026-07-29 14:59:25 +0530

    Delete week5 directory

--- week5/test_twttr.py
+++
@@ -1,9 +0,0 @@
-from twttr import shorten
-def test_lowercase():
-    assert shorten("twitter")== "twttr"
-def test_uppercase():
-    assert shorten("TWITTER")=="TWTTR"
-def test_numbers():
-    assert shorten ("CS50")== "CS50"
-def test_punctuation():
-    assert shorten("Hello,World!")=="Hll,Wrld!"
